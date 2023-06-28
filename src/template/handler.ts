65ed58cf56e56064a32b084c9d583341c5956d3f
ekrenz <ekrenz.developer@gmail.com>
2023-06-28 21:40:26 +0100

    refactor test

--- src/template/handler.ts
+++ src/template/handler.ts
@@ -1,42 +1,42 @@
-import { HttpStatus } from '@nestjs/common';
-import { NestFactory } from '@nestjs/core';
-import {
-  Callback,
-  Context,
-  Handler,
-  APIGatewayEvent,
-  APIGatewayProxyResult,
-} from 'aws-lambda';
+// import { HttpStatus } from '@nestjs/common';
+// import { NestFactory } from '@nestjs/core';
+// import {
+//   Callback,
+//   Context,
+//   Handler,
+//   APIGatewayEvent,
+//   APIGatewayProxyResult,
+// } from 'aws-lambda';
 
-import { AppModule } from '@src/app.module';
-import { TemplateService } from '@src/template/services/template.service';
+// import { AppModule } from '@src/app.module';
+// import { TemplateService } from '@src/template/services/template.service';
 
-export const helloWorld: Handler = async (
-  event: APIGatewayEvent
-  // context: Context,
-  // callback: Callback
-): Promise<APIGatewayProxyResult> => {
-  const appContext = await NestFactory.createApplicationContext(AppModule);
-  const service = appContext.get(TemplateService);
-  const response = service.getHello();
+// export const helloWorld: Handler = async (
+//   event: APIGatewayEvent
+//   // context: Context,
+//   // callback: Callback
+// ): Promise<APIGatewayProxyResult> => {
+//   const appContext = await NestFactory.createApplicationContext(AppModule);
+//   const service = appContext.get(TemplateService);
+//   const response = service.getHello();
 
-  return {
-    body: JSON.stringify(response),
-    statusCode: HttpStatus.OK,
-  };
-};
+//   return {
+//     body: JSON.stringify(response),
+//     statusCode: HttpStatus.OK,
+//   };
+// };
 
-export const byeWorld: Handler = async (
-  event: APIGatewayEvent
-  // context: Context,
-  // callback: Callback
-): Promise<APIGatewayProxyResult> => {
-  const appContext = await NestFactory.createApplicationContext(AppModule);
-  const service = appContext.get(TemplateService);
-  const response = service.getBye();
+// export const byeWorld: Handler = async (
+//   event: APIGatewayEvent
+//   // context: Context,
+//   // callback: Callback
+// ): Promise<APIGatewayProxyResult> => {
+//   const appContext = await NestFactory.createApplicationContext(AppModule);
+//   const service = appContext.get(TemplateService);
+//   const response = service.getBye();
 
-  return {
-    body: JSON.stringify(response),
-    statusCode: HttpStatus.OK,
-  };
-};
+//   return {
+//     body: JSON.stringify(response),
+//     statusCode: HttpStatus.OK,
+//   };
+// };
